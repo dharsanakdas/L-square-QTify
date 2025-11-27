08cdab23190bbc68bfbe75d7fe590858d94209b9
dharsana k das <dharsanakdas@gmail.com>
2025-11-27 14:10:21 +0530

    nav and hero

--- qtify/src/Components/Navbar/Navbar.jsx
+++ qtify/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import styles from "./Navbar.module.css";
-import Button from "../Button/Button";
 
-function Navbar(searchData) {
+function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
       <Link to="/">
@@ -15,7 +15,6 @@ function Navbar(searchData) {
         placeholder="Search a song of your choice"
         searchData={searchData}
       />
-
       <Button>Give Feedback</Button>
     </nav>
   );
